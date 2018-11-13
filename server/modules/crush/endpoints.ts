a641185ba790b9210a68226fd244251785dba091
Elmano Neto <elmanoneto@gmail.com>
2018-11-13 14:04:20 -0300

    define put and delete routes

--- server/modules/crush/endpoints.ts
+++ server/modules/crush/endpoints.ts
@@ -6,5 +6,7 @@ const router = Router()
 router.get('/api/crushes', CrushRoutes.getAll)
 router.get('/api/crushes/:id', CrushRoutes.getById)
 router.post('/api/crushes/', CrushRoutes.create)
+router.put('/api/crushes/:id', CrushRoutes.update)
+router.delete('/api/crushes/:id', CrushRoutes.delete)
 
 export default router
